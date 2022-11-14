c3b0760992e724611ddc98814f6f0655928e91f1
Janis Ostrovskis <janis.ostrovski@gmail.com>
2022-11-14 17:11:00 +0200

    LEARNING

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -51,7 +51,7 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1991);
 console.log(age1, age2);
-*/
+
 
 // Arrow function
 const calcAge3 = birthYear => 2037 - birthYear;
@@ -67,3 +67,112 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 }
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
+
+
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 23;
+}
+
+const fruitProcessor = (apples, oranges) => {
+    // console.log(apples, oranges);
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired 🎉`);
+        return -1;
+    }
+
+    // return retirement;
+    // return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+// Coding Challenge #1
+
+const averageScore = (score1, score2, score3) => (score1 + score2 + score3) / 3
+
+const avgDolphins = averageScore(85, 54, 41)
+const avgKoalas = averageScore(23, 34, 27)
+
+// console.log(avgDolphins, avgKoalas)
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins / 2 >= avgKoalas) {
+        console.log(`Dolphins win  (${avgDolphins} vs. ${avgKoalas})`)
+    } else if (avgKoalas / 2 >= avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+    } else {
+        console.log(`No one wins!`)
+    }
+}
+
+checkWinner(avgDolphins, avgKoalas)
+
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+
+
+
+// Exercise
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+
+*/
